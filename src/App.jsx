9636ace2040f98f6bde59c98c84d6a270dd060f2
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2024-10-10 18:17:01 +0300

    axios post tanimlandi

--- src/App.jsx
+++ src/App.jsx
@@ -20,41 +20,31 @@ function App() {
   return (
     <div className={`h-screen ${darkMode === "dark" ? "bg-black text-white" : "bg-white text-black"}`}>
       <nav className="w-full flex justify-end items-end p-4 dark:bg-black">
-
-
         <button
           onClick={changeDarkMode}
           className={`flex px-6 text-[#777777] font-bold font-[Inter] text-[20px] rounded-sm mr-20 ${darkMode === "dark" ? "text-white" : "text-black"}`}
         >
-        
           <img 
             src={darkMode === "dark" ? "/light-switch.png" : "/dark-switch.png"} 
             alt="Dark Mode Toggle" 
-            className="mr-7 mt-1 w-12 h-5 " 
+            className="mr-7 mt-1 w-12 h-5" 
           />
           Dark Mode |
         </button>
 
-
-
         <div>
-
-
-        <button 
-
-  onClick={() => changeLang(language === 'tr' ? 'en' : 'tr')} 
-  className="mx-2 relative right-[6rem]"
->
-  <span className="text-purple-500  font-bold font-[Inter] text-[20px]">
-    {language === 'tr' ? "INGILIZCE" : "TURKCE"}
-  </span>
-  <span className="text-[#777777]  font-bold font-[Inter] text-[20px]">
-    {language === 'tr' ? "'YE GEC" : "'YE GEC"}
-  </span>
-</button>
-
-</div>
-
+          <button 
+            onClick={() => changeLang(language === 'tr' ? 'en' : 'tr')} 
+            className="mx-2 relative right-[6rem]"
+          >
+            <span className="text-purple-500 font-bold font-[Inter] text-[20px]">
+              {language === 'tr' ? "INGILIZCE" : "TURKCE"}
+            </span>
+            <span className="text-[#777777] font-bold font-[Inter] text-[20px]">
+              {language === 'tr' ? "'YE GEC" : "'YE GEC"}
+            </span>
+          </button>
+        </div>
       </nav>
       <Header />
       <Hero />
--- src/contexts/LanguageProvider.jsx
+++ src/contexts/LanguageProvider.jsx
@@ -1,17 +1,36 @@
-import { createContext, useState, useContext } from "react";
-import { languageData } from "../content/langdata.js";
+import { createContext, useContext } from "react";
+import { languageData } from "../content/langdata";
+
+import useLocalStorage from '../hooks/useLocalStorage';
+import axios from 'axios'; 
 
 const LanguageContext = createContext();
+
 export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState("tr");
+  const [language, setLanguage] = useLocalStorage("language", "tr");
+
   function changeLang(lang) {
     setLanguage(lang);
+
+   
+    const data = { lang: lang === 'en' ? "EN" : "TR" };
+    
+    axios.post("https://reqres.in/api/workintech", data)
+      .then((response) => {
+        console.log("Dil başarıyla gönderildi:", response.data);
+      })
+      .catch((error) => {
+        console.error("Dil değiştirme sırasında hata oluştu:", error);
+      });
   }
+
   const texts = languageData[language];
+
   return (
     <LanguageContext.Provider value={{ language, changeLang, texts }}>
       {children}
     </LanguageContext.Provider>
   );
 };
+
 export const useLanguage = () => useContext(LanguageContext);
